e30adba5f2302172b43dc2fe98ce0ebe44c037a7
AsunaKKK <aiaicz99@gmail.com>
2023-04-12 18:23:22 +0700

    EditScene

--- Assets/Script/DestoryEx.cs
+++ Assets/Script/DestoryEx.cs
@@ -2,17 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class diesss : MonoBehaviour
+public class DestoryEx : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        Destroy(gameObject,0.5f);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Destroy(gameObject, 1.5f);
     }
 }
